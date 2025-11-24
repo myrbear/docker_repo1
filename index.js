e58ee40cc6f48232bb017afb8e30655cfd0e0316
myrbear <mzlafty@gmail.com>
2025-11-24 15:46:20 +0000

    functional

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const path=require('path')
+
+var myvalue="dog";
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/one", (req, res) => {res.render('./one.html')})
+app.post("/two", function(req, res){ 
+	var animal=req.param("myvalue");
+	console.log(animal)
+	res.render("two.html", {animal:animal});
 });
 
 app.listen(PORT, () => {
